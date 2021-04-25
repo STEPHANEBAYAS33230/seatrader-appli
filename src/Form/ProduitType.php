b706c27a75cbeec67d37e1aae05766d951d2c559
Stephane Mounier <lila74500@hotmail.fr>
2021-04-25 14:35:53 +0200

    ajouter article oki doki

---
+++ src/Form/ProduitType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\FamilleProduit;
+use App\Entity\PieceOuKg;
+use App\Entity\Produit;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProduitType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nomProduit')
+            //->add('quantite')
+            ->add('famille', EntityType::class,['class' =>FamilleProduit::class,'choice_value'=>'id'])
+            ->add('pieceOuKg', EntityType::class, ['class' => PieceOuKg::class,'choice_value'=>'id'])
+
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Produit::class,
+        ]);
+    }
+}
