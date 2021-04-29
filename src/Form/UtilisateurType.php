ffb75a767a0269fd6b9390fd99a4186bf38e1f7c
Stephane Mounier <lila74500@hotmail.fr>
2021-04-29 21:21:24 +0200

    formulaire creer utilisateut

--- src/Form/UtilisateurType.php
+++ src/Form/UtilisateurType.php
@@ -9,6 +9,7 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SearchType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -17,22 +18,22 @@ class UtilisateurType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nomDeLaSociete')
+            ->add('nomDeLaSociete',SearchType::class,['attr'=>['class'=>'form-control text-primary', 'value'=>'', 'style'=> 'background-color: #fffcc8; border-radius: 15px;', 'placeholder'=>'nom Société',]])
             //->add('roles')
             ->add('password', RepeatedType::class,[
                 'type' => PasswordType::class,
                 'invalid_message' => 'The password fields must match.',
-                'options' => ['attr' => ['class' => 'password-field']],
+                 'options' => ['attr' => ['class' => 'password-field form-control text-primary', 'value'=> '', 'placeholder'=>'mot de passe', 'style'=>'background-color: #fffcc8; border-radius: 15px;']],
                 'required' => true,
                 'first_options'  => ['label' => 'Password'],
                 'second_options' => ['label' => 'Repeat Password'],
             ])
-            ->add('nom')
-            ->add('prenom')
-            ->add('emailSociete')
-            ->add('emailPerso')
-            ->add('telephone_societe')
-            ->add('telephonePerso')
+            ->add('nom',SearchType::class,['attr'=>['class'=>'form-control text-primary', 'value'=>'', 'style'=> 'background-color: #fffcc8; border-radius: 15px;', 'placeholder'=>'nom',]])
+            ->add('prenom',SearchType::class,['attr'=>['class'=>'form-control text-primary', 'value'=>'', 'style'=> 'background-color: #fffcc8; border-radius: 15px;', 'placeholder'=>'prenom',]])
+            ->add('emailSociete',SearchType::class,['attr'=>['class'=>'form-control text-primary', 'value'=>'', 'style'=> 'background-color: #fffcc8; border-radius: 15px;', 'placeholder'=>'email Société',]])
+            ->add('emailPerso',SearchType::class,['attr'=>['class'=>'form-control text-primary', 'value'=>'', 'style'=> 'background-color: #fffcc8; border-radius: 15px;', 'placeholder'=>'email personnel',]])
+            ->add('telephone_societe',SearchType::class,['attr'=>['class'=>'form-control text-primary', 'value'=>'', 'style'=> 'background-color: #fffcc8; border-radius: 15px;', 'placeholder'=>'tel Société',]])
+            ->add('telephonePerso',SearchType::class,['attr'=>['class'=>'form-control text-primary', 'value'=>'', 'style'=> 'background-color: #fffcc8; border-radius: 15px;', 'placeholder'=>'tel personnel',]])
             //->add('etatUilisateur')
             //->add('etatUtilisateur', EntityType::class, ['class' => EtatUtilisateur::class,'choice_value'=>'etatUtilisateur'])
 
