b66b343e58a10e1e4584ee8d459364fb4b4766b4
Stephane Mounier <lila74500@hotmail.fr>
2021-05-11 14:29:36 +0200

    MODIFIER CDE OK

--- src/Controller/CommandeController.php
+++ src/Controller/CommandeController.php
@@ -38,7 +38,7 @@ class CommandeController extends AbstractController
 
         // creer une formulaire
         //************formulaire
-        $cde=new Commande();;
+        $cde=new Commande();
         $commande = new Commande();
 
         //*******recup datejour et user
@@ -69,12 +69,13 @@ class CommandeController extends AbstractController
             //$dtt=$dateVoulue;
             // tableau des jours de la semaine date_format($date,"Y/m/d H:i:s");
             //$dtt=date("d/m/Y");
+            //********connaitre le jour de la semaine
             $joursem = array('dim', 'lun', 'mar', 'mer', 'jeu', 'ven', 'sam');
-// extraction des jour, mois, an de la date
+            // extraction des jour, mois, an de la date
             list($jour, $mois, $annee) = explode('/', $dtt);
-// calcul du timestamp
+            // calcul du timestamp
             $timestamp = mktime (0, 0, 0, $mois, $jour, $annee);
-// affichage du jour de la semaine
+            // affichage du jour de la semaine
             $jourSem= $joursem[date("w",$timestamp)];
             //*********si jour livraison egale à dim ou lundi pas de cde possible
             //******************************
@@ -301,8 +302,8 @@ class CommandeController extends AbstractController
     /**
      * @Route("/commande-/{id}", name="supprimer-cde")
      */
-    public function supprimerMiseEnAvant($id, EntityManagerInterface $em){
-        //****************on recupere la miseEnAvant
+    public function supprimerCde($id, EntityManagerInterface $em){
+        //****************on recupere la cde
         $cdeRepo = $this->getDoctrine()->getRepository(Commande::class);
         $cde = $cdeRepo->find($id);
 
@@ -310,5 +311,249 @@ class CommandeController extends AbstractController
         $em->remove($cde);
         $em->flush();
         return $this->redirectToRoute('voir_cde');
+    }
+
+
+    //********************supprimer mise en avant
+    /**
+     * @Route("/commande-modifier/{id}", name="modifier-cde")
+     */
+    public function modifierCde($id, Request $request, EntityManagerInterface $em)
+    {    // on récupère l'user/ date today
+        $user=$this->getUser();
+        $today = new \DateTime('now');
+        // recupere toutes les familles
+        $familleProduitRepo = $this->getDoctrine()->getRepository(FamilleProduit::class);
+        $familleProduit = $familleProduitRepo->findAll();
+        //***CDE
+        $cde=new Commande();
+        //****************on recupere la cde
+        $cdeRepo = $this->getDoctrine()->getRepository(Commande::class);
+        $commande = $cdeRepo->find($id);
+        $produits=$commande->getObject();
+        $this->addFlash('error', "Vous pouvez modifier cette commande.");
+        $commandeForm = $this->createForm(CommandeType::class, $commande);
+        $commandeForm->handleRequest($request);
+        //***********************************soumission du formulaire
+        if ($commandeForm->isSubmitted() and $commandeForm->isValid()) {
+            //*******verif des dates et ouverture des cde
+            $dateVoulue=($commande->getJourDeLivraison());
+            $dateVoulue2=($commande->getJourDeLivraison());
+
+            //******************** jour en francais
+            $dtt=date_format($dateVoulue,"d/m/Y");
+            $dtt2=date_format($dateVoulue2,"Y-m-d");
+
+            //$dtt=$dateVoulue;
+            // tableau des jours de la semaine date_format($date,"Y/m/d H:i:s");
+            //$dtt=date("d/m/Y");
+            //********connaitre le jour de la semaine
+            $joursem = array('dim', 'lun', 'mar', 'mer', 'jeu', 'ven', 'sam');
+            // extraction des jour, mois, an de la date
+            list($jour, $mois, $annee) = explode('/', $dtt);
+            // calcul du timestamp
+            $timestamp = mktime (0, 0, 0, $mois, $jour, $annee);
+            // affichage du jour de la semaine
+            $jourSem= $joursem[date("w",$timestamp)];
+            //*********si jour livraison egale à dim ou lundi pas de cde possible
+            //******************************
+            //** MANQUE VOIR SI DATE OUVERTE(pour dim/et lun) ou bloqué(pour mar/mer/jeu/ven/sam) PAR ADMIN
+            //******************************
+            if ($jourSem=="dim" or $jourSem=="lun") {
+                $this->addFlash('error', "La livraison n'est pas ouverte pour ce jour là.");
+                $commandeForm = $this->createForm(CommandeType::class, $commande);
+                $commandeForm->handleRequest($request);
+                foreach( $produits as $prd ) {
+                    $prd->setQuantite($request->request->get('prod'.(string)$prd->getId(),0));
+
+                }
+                return $this->render('commande/index.html.twig',[ "dateToday"=>$today,"user"=>$user, "commandeForm"=>$commandeForm->createView(),
+                    "familleProduit"=>$familleProduit, 'produits'=>$produits,'cde'=>$cde,
+
+                ]);
+            }
+            //**********controle si date du jour <24h date de liv
+            $dttjour=date("d");
+            $dttmois=date("m");
+            $dttan=date("Y");
+            $dtjour=date("d/m/Y");
+            $dtjour2=new \DateTime('now');
+            $dtjour2=date_format($dtjour2,"Y-m-d");
+            //$diff = round((strtotime($dtt) - strtotime($dtjour))/(60*60*24)-1);
+            $heure=date('H');
+
+            //calcul de la diff
+            // jour mois annee date de liv choisie
+            $start = new DateTimeImmutable($dtjour2);//date de départ
+            $end = new DateTimeImmutable($dtt2);//date de départ
+            $interval = $start->diff($end);//on récupère la différence entre ces 2 dates
+            $diff=$interval->format('%a');//affiche : en jours
+            if(intval($jour)<intval($dttjour) and intval($mois)==intval($dttmois) and intval($annee)==intval($dttan)){
+                $diff=$diff*-1;
+            } else  if( intval($mois)<intval($dttmois) and intval($annee)==intval($dttan)) { $diff=$diff*-1;}
+            else  if( intval($annee)<intval($dttan)) { $diff=$diff*-1;}
+            /*var_dump($dttjour);
+            var_dump($dttmois);
+            var_dump($dttan);
+            var_dump($dtjour);
+            var_dump($diff);
+            var_dump($jourSem);
+            var_dump($jour);
+            var_dump($mois);
+            var_dump($annee);
+            var_dump($heure);
+            die();*/
+            //*******************************
+            if ($diff<=0) {
+                $this->addFlash('error', "Erreur Date de livraison.");
+
+                $commandeForm = $this->createForm(CommandeType::class, $commande);
+                $commandeForm->handleRequest($request);
+                foreach( $produits as $prd ) {
+                    $prd->setQuantite($request->request->get('prod'.(string)$prd->getId(),0));
+
+                }
+                return $this->render('commande/index.html.twig',[ "dateToday"=>$today,"user"=>$user, "commandeForm"=>$commandeForm->createView(),
+                    "familleProduit"=>$familleProduit, 'produits'=>$produits,'cde'=>$cde,
+
+                ]);
+            }
+            //**************erreur si livraison sup à 1mois
+
+            if ($diff>31) {
+                $this->addFlash('error', "Date de livraison >à 1mois.");
+                $commandeForm = $this->createForm(CommandeType::class, $commande);
+                $commandeForm->handleRequest($request);
+                foreach( $produits as $prd ) {
+                    $prd->setQuantite($request->request->get('prod'.(string)$prd->getId(),0));
+
+                }
+                return $this->render('commande/index.html.twig',[ "dateToday"=>$today,"user"=>$user, "commandeForm"=>$commandeForm->createView(),
+                    "familleProduit"=>$familleProduit, 'produits'=>$produits,'cde'=>$cde,
+
+                ]);
+            }
+            //*********erreur si livraison j+1 et apres 11h
+            if ($diff==1 and  intval($heure)>10) {
+                $this->addFlash('error', "heure dépassée pour livraison le lendemain (avant 11h)");
+                $commandeForm = $this->createForm(CommandeType::class, $commande);
+                $commandeForm->handleRequest($request);
+                foreach( $produits as $prd ) {
+                    $prd->setQuantite($request->request->get('prod'.(string)$prd->getId(),0));
+
+                }
+                return $this->render('commande/index.html.twig',[ "dateToday"=>$today,"user"=>$user, "commandeForm"=>$commandeForm->createView(),
+                    "familleProduit"=>$familleProduit, 'produits'=>$produits,'cde'=>$cde,
+
+                ]);
+            }
+            //********erreur de date inf à la date du jour
+            if (intval($dttjour)>=intval($jour) and intval($dttmois)==intval($mois) and intval($dttan)==intval($annee) ) {
+                $this->addFlash('error', "Problème de date de livraison.");
+                $commandeForm = $this->createForm(CommandeType::class, $commande);
+                $commandeForm->handleRequest($request);
+                foreach( $produits as $prd ) {
+                    $prd->setQuantite($request->request->get('prod'.(string)$prd->getId(),0));
+
+                }
+                return $this->render('commande/index.html.twig',[ "dateToday"=>$today,"user"=>$user, "commandeForm"=>$commandeForm->createView(),
+                    "familleProduit"=>$familleProduit, 'produits'=>$produits,'cde'=>$cde,
+
+                ]);
+            }
+            if (intval($dttmois)>intval($mois) and intval($dttan)==intval($annee) ) {
+                $this->addFlash('error', "Problème de date de livraison.");
+                $commandeForm = $this->createForm(CommandeType::class, $commande);
+                $commandeForm->handleRequest($request);
+                foreach( $produits as $prd ) {
+                    $prd->setQuantite($request->request->get('prod'.(string)$prd->getId(),0));
+
+                }
+                return $this->render('commande/index.html.twig',[ "dateToday"=>$today,"user"=>$user, "commandeForm"=>$commandeForm->createView(),
+                    "familleProduit"=>$familleProduit, 'produits'=>$produits,'cde'=>$cde,
+
+                ]);
+            }
+            if (intval($dttan)>intval($annee) ) {
+                $this->addFlash('error', "Problème de date de livraison.");
+                $commandeForm = $this->createForm(CommandeType::class, $commande);
+                $commandeForm->handleRequest($request);
+                foreach( $produits as $prd ) {
+                    $prd->setQuantite($request->request->get('prod'.(string)$prd->getId(),0));
+
+                }
+                return $this->render('commande/index.html.twig',[ "dateToday"=>$today,"user"=>$user, "commandeForm"=>$commandeForm->createView(),
+                    "familleProduit"=>$familleProduit, 'produits'=>$produits,'cde'=>$cde,
+
+                ]);
+            }
+
+
+
+            // recupere toutes les produits
+            $produitRepo = $this->getDoctrine()->getRepository(Produit::class);
+            $produits = $produitRepo->findAll();
+
+            foreach( $produits as $prd ) {//recupere les valeurs des input en formulaire pour les produits
+                $prd->setQuantite($request->request->get('prod'.(string)$prd->getId(),0));
+                //voir si tout produit quantite egal à 0 pour ne pas enregistrer
+                //$commande->add($prd);
+                //$prd->addcommande($commande);
+                // recupere l etat envoyee
+                $etatRepo = $this->getDoctrine()->getRepository(EtatCommande::class);
+                $etat = $etatRepo->find(2);
+
+                $commande->setEtatCommande($etat);
+                $commande->setObject($produits);
+                //$em->persist($prd); pâs d'enregistrement de la quantite ds l'entity produit (doit toujours etre egal à zero)
+            }
+            $pasVide=false;//****voir si cde vide en quantite
+            foreach ($commande->getObject() as $lobjet)
+            {
+                if ($lobjet->getQuantite()>0){$pasVide=true;}
+            }
+            //*****si cde vide retour en page de cde
+            if ( $pasVide==false ) {
+                $this->addFlash('error', "La commande est vide.");
+                $commandeForm = $this->createForm(CommandeType::class, $commande);
+                $commandeForm->handleRequest($request);
+                foreach( $produits as $prd ) {
+                    $prd->setQuantite($request->request->get('prod'.(string)$prd->getId(),0));
+
+                }
+                return $this->render('commande/index.html.twig',[ "dateToday"=>$today,"user"=>$user, "commandeForm"=>$commandeForm->createView(),
+                    "familleProduit"=>$familleProduit, 'produits'=>$produits,'cde'=>$cde,
+
+                ]);
+            }
+            //****************************
+            //*********controle securité si user connecté est egal à l auteur de la commande
+            $utilisateur=$commande->getUtilisateur();
+            $idUtilisateur=$utilisateur->getId();
+            if ($idUtilisateur=$user->getId()){
+
+                //**************************
+                //***on enregistre la cde
+                $em->persist($commande);
+                $em->flush();
+
+
+                //***********redirection vers voir mes commandes
+                $this->addFlash('success', "Commande enregistrée et envoyée");
+
+                return $this->redirectToRoute('voir_cde');
+
+            } else {
+                $this->addFlash('error', "Un problème a été détecté. La commande n'a pas été enregistré.");
+            }
+        }
+
+        //***********************************
+        return $this->render('commande/index.html.twig',[ "dateToday"=>$today,"user"=>$user, "commandeForm"=>$commandeForm->createView(),
+            "familleProduit"=>$familleProduit, 'produits'=>$produits,'cde'=>$commande,
+
+        ]);
+
+
     }
 }
